1ad3751037cab921bee5a955e58bf1e72880585d
Idriss Chaouch <62848804+CppProgrammer23@users.noreply.github.com>
2020-07-10 15:33:10 +0100

    Update mainConvertToChar.cpp

--- ConvertFrom-ToChar/mainConvertToChar.cpp
+++ ConvertFrom-ToChar/mainConvertToChar.cpp
@@ -8,9 +8,15 @@ int main()
 	std::array<char, 10> str{};
 	to_char_res(str, val);
 	
-	// or directly this:
+	// or directly this without need to "convertToChar.h"
 	/*if (auto [p, ec] = std::to_chars(str.data(), str.data() + str.size(), val); ec == std::errc())
 		std::cout << "the string is: " << std::string_view(str.data(), p - str.data()); */
+	
+	//2nd version
+	int val = 1724;
+	char str1[15];
+	to_char_res(str1, val);
+	std::cout << "the value in string is: " << str1;
 
 	return 0;
 }
