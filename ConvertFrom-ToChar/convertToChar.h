10eaf9c25d8a9a34122952b46031fd15ee1613df
Idriss Chaouch <62848804+CppProgrammer23@users.noreply.github.com>
2020-07-10 15:31:22 +0100

    Update convertToChar.h

--- ConvertFrom-ToChar/convertToChar.h
+++ ConvertFrom-ToChar/convertToChar.h
@@ -3,7 +3,8 @@
 #include <charconv>
 #include <array>
 #include <type_traits>
-
+//1 #pragma warning (disable:4996)  //activate 1 and 2 if you're using the second version
+//2 #include <stdlib.h>
 
 //convert from arithmetic to chars
 template<typename T, std::enable_if_t<std::is_arithmetic_v<T>, bool> = true>
@@ -11,4 +12,11 @@ template<typename T, std::enable_if_t<std::is_arithmetic_v<T>, bool> = true>
 {
 	if (auto [p, ec] = std::to_chars(str.data(), str.data() + str.size(), val);ec == std::errc())
 		std::cout << "the string is: " << std::string_view(str.data(), p - str.data());
+	
+	//2nd version
+	/*if(std::is_integral_v<T>)
+		sprintf(str, "%d", val);
+	else
+		sprintf(str, "%.5f", val);*/
+
 }
